8c49ca37553e30728831a848eed4ec3aa7ab7f01
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2022-10-02 23:12:39 +0530

    Create Java_program_to_print_the_pattern.java

---
+++ Day4/Java_program_to_print_the_pattern.java
@@ -0,0 +1,15 @@
+public class Main
+{
+	public static void main(String[] args) {
+	int n=4;
+	char ch='A';
+	for(int i =1;i<=n;i++){
+	    for(int chars=1;chars<=i;chars++){
+	        System.out.print(ch);
+	        ch++;
+	    }System.out.println();
+	}
+	
+	
+	}
+}
