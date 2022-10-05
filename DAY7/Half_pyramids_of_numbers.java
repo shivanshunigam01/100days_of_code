fdd7fa4e2b95007f019a7fd1d12466fdd6b0f66a
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2022-10-05 21:38:40 +0530

    Creating_Half_pyramids_of_numbers

---
+++ DAY7/Half_pyramids_of_numbers.java
@@ -0,0 +1,15 @@
+public class Main
+{
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+		for(int i=1;i<=6;i++)
+		{
+            for(int j=1;j<=i;j++)
+		    {
+		        System.out.print(" "+i);
+		        
+		    }
+		    System.out.println();
+		}
+	}
+}
