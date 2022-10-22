0d1c62a1e456ddb34d4c8484c2000bb761a5e907
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2022-10-22 21:29:15 +0530

    <<<Output is here>>>
    A
    BC
    DEF
    GHIJ

--- Day23/Pattern.java
+++ Day23/Pattern.java
@@ -1,1 +1,21 @@
-//hello everyone stay tuned......
+//TASK- Write a java program to print the following pattern:
+/*
+A
+BC
+DEF
+GHIJ */
+public class Main
+{
+	public static void main(String[] args) {
+	int n=4;
+	char ch='A';
+	for(int i =1;i<=n;i++){
+	    for(int chars=1;chars<=i;chars++){
+	        System.out.print(ch);
+	        ch++;
+	    }System.out.println();
+	}
+	
+	
+	}
+
