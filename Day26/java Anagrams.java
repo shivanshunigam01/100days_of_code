4808b30e3146555722e97c378bb2fed4ceaa717d
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2022-10-25 00:00:33 +0530

    <<<SAMPLE OUTPUT/INPUT>>>
    1)
     Input: str1 = “listen”  str2 = “silent”
    Output: “Anagram”
    Explanation: All characters of “listen” and “silent” are the same.
    
    2)
    Input: str1 = “gram”  str2 = “arm”
    Output: “Not Anagram”

--- Day26/java Anagrams.java
+++ Day26/java Anagrams.java
@@ -1,2 +1,46 @@
-// happy diwali 
+/*TASK-Given two strings. The task is to check whether given strings are anagrams of each other or not. 
+An anagram of a string is another string that contains the same characters, only the order of characters can be different. 
+For example, “abcd” and “dabc” are an anagram of each other. */
+import java.util.Scanner;
+
+public class Main {
+
+    static boolean isAnagram(String s1, String s2) {
+        // Complete the function
+        s1=s1.toLowerCase();
+        s2=s2.toLowerCase();
+        
+        if(s1.length()==s2.length())
+
+        {
+            int[] a = new int[256];
+            int[] b = new int[256];
+            for (int i = 0; i < s1.length(); i++) {
+                a[(int) s1.charAt(i)] += 1;
+                b[(int) s2.charAt(i)] += 1;
+            }
+            for (int i = 0; i < 256; i++) {
+                if (a[i] != b[i])
+                    return false;
+
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
+
 
