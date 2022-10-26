064c8f6a92e5c314f401d53670ef60c5446d9231
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2022-10-26 23:24:26 +0530

    <<<Sample input/Output>>>
    Sample Input:
    12324.134
    __________________________________
    Sample Output:
    US: $12,324.13
    India: Rs.12,324.13
    China: ￥12,324.13
    France: 12 324,13 €
    _____________________________________
    Explanation:
    Each line contains the value of payment formatted according to the four countries' respective currencies.
    _____________________________________

--- Day28/Java currency converter.java
+++ Day28/Java currency converter.java
@@ -1,1 +1,33 @@
-//this wil be completed today
+/*Given a double-precision number,payment , denoting an amount of money,
+use the NumberFormat class' getCurrencyInstance method to convert payment into the US, Indian, Chinese, and French currency formats. 
+Then print the formatted values as follows:
+/*
+import java.util.*;
+import java.text.*;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        
+        // Write your code here.
+        /* Create custom Locale for India. 
+          I used the "IANA Language Subtag Registry" to find India's country code */
+        Locale indiaLocale = new Locale("en", "IN");
+
+        /* Create NumberFormats using Locales */
+        NumberFormat us     = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat india  = NumberFormat.getCurrencyInstance(indiaLocale);
+        NumberFormat china  = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+
+        /* Print output */        
+        System.out.println("US: "     + us.format(payment));
+        System.out.println("India: "  + india.format(payment));
+        System.out.println("China: "  + china.format(payment));
+        System.out.println("France: " + france.format(payment));
+    
+    }
+}
