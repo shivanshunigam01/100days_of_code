8ae50379d3fe97a2ca513097d1af771971afc719
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2022-11-02 00:35:29 +0530

    <<<java program using try,catch,and finally keywords>>>

--- Day34/Java program using try,cache,finally keyword.java
+++ Day34/Java program using try,cache,finally keyword.java
@@ -1,1 +1,13 @@
-//hey 
+//java program using try,catch,and finally keywords.....
+public class Main {
+  public static void main(String[] args) {
+    try {
+      int[] myNumbers = {1, 2, 3};
+      System.out.println(myNumbers[10]);
+    } catch (Exception e) {
+      System.out.println("Something went wrong.");
+    } finally {
+      System.out.println("The 'try catch' is finished.");
+    }
+  }
+}
