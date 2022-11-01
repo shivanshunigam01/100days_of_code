ddae55276775b66a6531a87310e56bc763813cb3
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2022-11-01 22:55:59 +0530

    <<<Overloading by changing the number of parameters>>>

--- Day33/Method overloading.java
+++ Day33/Method overloading.java
@@ -1,1 +1,15 @@
-//Have pateince guyzz.............................
+// Overloading by changing the number of parameters
+class MethodOverloading {
+    private static void display(int a){
+        System.out.println("Arguments: " + a);
+    }
+
+    private static void display(int a, int b){
+        System.out.println("Arguments: " + a + " and " + b);
+    }
+
+    public static void main(String[] args) {
+        display(1);
+        display(1, 4);
+    }
+}
