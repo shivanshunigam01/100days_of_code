39fb0e9cda531a1563665c6b18f12046697166b0
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2022-11-02 22:47:54 +0530

    <<<Introduction to throw keyword>>>
    The throw keyword
    The throw statement allows you to create a custom error.
    
    The throw statement is used together with an exception type. There are many exception types available in Java: ArithmeticException, FileNotFoundException, ArrayIndexOutOfBoundsException, SecurityException, etc:

---
+++ Day35/Java Exceptions using throw keyword.java
@@ -0,0 +1,17 @@
+/*Example
+Throw an exception if age is below 18 (print "Access denied"). If age is 18 or older, print "Access granted":*/
+public class Main {
+  static void checkAge(int age) {
+    if (age < 18) {
+      throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+    }
+    else {
+      System.out.println("Access granted - You are old enough!");
+    }
+  }
+
+  public static void main(String[] args) {
+    checkAge(15); // Set age to 15 (which is below 18...)
+  }
+}
+ 
