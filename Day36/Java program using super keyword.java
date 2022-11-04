8c79ebc9a635cf19c3cccf8dcf94bba9dd4f9bca
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2022-11-04 07:19:23 +0530

    <<<Super keyword with variables>>>

--- Day36/Java program using super keyword.java
+++ Day36/Java program using super keyword.java
@@ -1,1 +1,42 @@
-//hey
+// Java code to show use of super keyword with variables
+// Base class vehicle
+
+class Vehicle {
+
+    int maxSpeed = 120;
+}
+ 
+// sub class Car extending vehicle
+
+class Car extends Vehicle {
+
+    int maxSpeed = 180;
+ 
+
+    void display()
+
+    {
+
+        // print maxSpeed of base class (vehicle)
+
+        System.out.println("Maximum Speed: "
+
+                           + super.maxSpeed);
+
+    }
+}
+ 
+// Driver Program
+
+class Test {
+
+    public static void main(String[] args)
+
+    {
+
+        Car small = new Car();
+
+        small.display();
+
+    }
+}
