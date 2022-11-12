9520041949bd1d09b53c9d16a357f289cf3cd4f4
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2022-11-13 00:47:22 +0530

    <<<Hackerank java standard input and output program>>>

--- Day45/Java standard input output.java
+++ Day45/Java standard input output.java
@@ -1,1 +1,18 @@
-//hey 
+//The code above creates a Scanner object named Scanner and uses it to read a String and an int. 
+ //It then closes the Scanner object because there is no more input to read, and prints to stdout using
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+}
