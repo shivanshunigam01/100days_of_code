0a3cde06d8ac4845124a0f8fecee8b2b6a7852df
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2022-11-14 00:19:10 +0530

    <<<String reverse in java>>>

--- Day46/String reverse in java.java
+++ Day46/String reverse in java.java
@@ -1,1 +1,32 @@
-//hey
+// Java program to demonstrate conversion from 
+// String to StringBuffer and reverse of string 
+
+import java.lang.*;
+
+import java.io.*; 
+
+import java.util.*; 
+
+  
+
+public class String_reverse {
+    public static void main(String[] args) 
+
+    { 
+
+        String str = "ABCDEFGH"; 
+
+  
+
+        // conversion from String object to StringBuffer 
+
+        StringBuffer sbr = new StringBuffer(str); 
+
+        // To reverse the string 
+
+        sbr.reverse(); 
+
+        System.out.println(sbr); 
+
+    } 
+}
