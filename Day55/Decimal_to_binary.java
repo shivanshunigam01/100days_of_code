c968086dafa196a6ea78a074a3f47f1f12deb560
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2022-11-23 18:21:25 +0530

    <<<Decimal to binary conversion in java >>>

---
+++ Day55/Decimal_to_binary.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Decimal_to_binary{
+	public static void main(String[] args) {	
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter the Decimal number:");
+	int Decimal = sc.nextInt();	
+	System.out.println(Integer.toBinaryString(Decimal));
+
+}
+
+	}
