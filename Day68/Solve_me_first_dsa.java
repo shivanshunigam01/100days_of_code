3c33359705f8c8d4fcfd0be39b7d0bb4b7da29e0
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2022-12-05 21:50:58 +0530

    <<<Solve me first (DSA )>>>
    In this HackerRank Solve Me First problem solution Complete the function solveMeFirst to compute the sum of two integers.
    
    Example
    
    a = 7
    
    b = 3
    
    Return 10.
    
    Function Description
    
    Complete the solveMeFirst function in the editor below.
    
    solveMeFirst has the following parameters:
    
    int a: the first value
    int b: the second value
    Returns
    
    - int: the sum of a and b

---
+++ Day68/Solve_me_first_dsa.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int solveMeFirst(int a, int b) {
+        return a+b;
+   }
+
+   
+ public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int _a = in.nextInt();
+        int _b = in.nextInt();
+        int sum = solveMeFirst(_a, _b);
+        System.out.println(sum);
+   }
+}
