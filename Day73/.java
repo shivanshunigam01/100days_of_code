a1848e1465a3a2e33ed6d18ace87a5a008301364
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2022-12-11 20:56:52 +0530

    <<<Description is here >>>
    In this HackerRank Staircase problem solution,Staircase detail
    
    This is a staircase of size n=4:
    
       #
    
      ##
    
     ###
    
    ####
    
    Its base and height are both equal to n. It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
    
    Write a program that prints a staircase of size n.
    
    Function Description
    
    Complete the staircase function in the editor below.
    
    staircase has the following parameter(s):
    
    int n: an integer
    
    Print
    
    Print a staircase as described above.
    
    Input Format
    
    A single integer, n, denoting the size of the staircase.
    
    Constraints
    
    0 < n <= 100
    
    Output Format
    
    Print a staircase of size n using # symbols and spaces.
    
    Note: The last line must have 0 spaces in it.

--- Day73/.java
+++ Day73/.java
@@ -1,1 +1,29 @@
-dfkmnflfnrfwfrojf nlwj;fn
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    
+    public static void drawStair(int size) {
+        int level = size-1;
+        for(int i = 0; i < size; i++) {
+            StringBuilder s = new StringBuilder();
+            for(int k = 0; k < level; k++) {
+                s.append(" ");
+            }
+            for(int k = 0; k < size - level; k ++){
+                s.append("#");
+            }
+            level -= 1;
+            System.out.println(s.toString());
+        }
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        Solution sol = new Solution();
+        
+        int size = in.nextInt();
+        sol.drawStair(size);
+    }
+}
