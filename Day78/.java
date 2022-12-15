474d155eb8eacabe0dba90e145fcc788e6b72d5a
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2022-12-15 21:54:34 +0530

    <<<Create file using (file handling)>>>

--- Day78/.java
+++ Day78/.java
@@ -1,1 +1,27 @@
-
+//Create file using (file handling)
+import java.io.File;  
+import java.io.IOException;  
+public class CreateFileExample1   
+{  
+public static void main(String[] args)   
+{     
+File file = new File("C:\\Users\\91989\\Desktop\\Filedemo1.txt"); //initialize File object and passing path as argument  
+boolean result;  
+try   
+{  
+result = file.createNewFile();  //creates a new file  
+if(result)      // test if successfully created a new file  
+{  
+System.out.println("file created "+file.getCanonicalPath()); //returns the path string  
+}  
+else  
+{  
+System.out.println("File already exist at location: "+file.getCanonicalPath());  
+}  
+}   
+catch (IOException e)   
+{  
+e.printStackTrace();    //prints exception if any  
+}         
+}  
+} 
