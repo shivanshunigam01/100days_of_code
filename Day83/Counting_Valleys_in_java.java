821db524bad44be59ff027c7f9b21554aa77253d
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2022-12-22 09:53:37 +0530

    <<<Counting valleys (Hackerrank solution))>>>

---
+++ Day83/Counting_Valleys_in_java.java
@@ -0,0 +1,28 @@
+/*In this HackerRank Counting Valleys problem in the Interview preparation kit you have Given the sequence of up and down steps during a hike, 
+find and print the number of valleys walked through*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        in.nextInt();
+        String s = in.next();
+        
+        int level = 0;
+        int valleys = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'U'){
+                level++;
+            }else if(s.charAt(i) == 'D'){
+                if(level == 0){
+                    valleys++;
+                }
+                level--;
+            }
+        }
+        System.out.println(valleys);
+    }
+}
