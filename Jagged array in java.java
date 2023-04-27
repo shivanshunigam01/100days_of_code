5ac20ada1dbd4b73ecf4a15d51172c7528cfa27b
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-04-27 22:20:05 +0530

    Update Jagged array in java.java

--- Jagged array in java.java
+++ Jagged array in java.java
@@ -16,6 +16,7 @@ class TestJaggedArray{
         for (int i=0; i<arr.length; i++){  
             for (int j=0; j<arr[i].length; j++){  
                 System.out.print(arr[i][j]+" ");  
+                
             }  
             System.out.println();//new line  
         }  
